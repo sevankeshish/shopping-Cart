d96e17d16fd98251750555d625136672db2e3353
Sevan Keshish <35175803+sevankeshish@users.noreply.github.com>
2021-10-24 13:11:41 +0330

    changes

--- shoppingcart/js/tamrin.js
+++ shoppingcart/js/tamrin.js
@@ -9,15 +9,19 @@ function eventListeners (){
 
 
 //function
+//add the course to the cart
 function buyCourse(e){
     e.preventDefault();
+    //use delegation to access to the course that selected
     if(e.target.classList.contains('add-to-cart')){
+        //access to the card div with parentElement
         const course = e.target.parentElement.parentElement
+        //read values
         getCourseInfo(course)
     }
 }
 
-//dastresi be etelaat dore
+//getting the course info that selected by user
 function getCourseInfo(course){
     console.log(course);
 }
